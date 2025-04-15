a5fba626c989a5c3a033cfd5b947bcde1c106830
Julio Machado <juliocarlos00@hotmail.com>
2025-04-15 23:42:06 +0100

    add/ create-account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,37 @@
+import { ConflictException } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Controller("/accounts")
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    const hashedPassord = await hash(password, 8);
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        "User with same e-mail address already exists."
+      );
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassord,
+      },
+    });
+  }
+}

933e2c239fc8a6068b85b4e492d02c30b5b90a58
Julio Machado <juliocarlos00@hotmail.com>
2025-04-18 02:19:03 +0100

    add: vitest

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -8,9 +8,9 @@ import {
 } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { compare } from "bcryptjs";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
 
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "@/prisma/prisma.service";
 import { z } from "zod";
 
 const authenticateBodySchema = z.object({

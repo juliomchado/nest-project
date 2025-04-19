ef0c84acf431886014d5363b20dab8cc2f372c99
Julio Machado <juliocarlos00@hotmail.com>
2025-04-19 01:38:05 +0100

    add: comunicate with domain and using create question use case

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -5,11 +5,15 @@ import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-q
 import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachments-repository";
 import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question-attachments-repository";
 import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 
 @Module({
   providers: [
     PrismaService,
-    PrismaQuestionsRepository,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswerAttachmentsRepository,
@@ -18,7 +22,7 @@ import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-ans
   ],
   exports: [
     PrismaService,
-    PrismaQuestionsRepository,
+    QuestionsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswerAttachmentsRepository,
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,7 +3,8 @@ import { CreateAccountController } from "./controllers/create-account.controller
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
 import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
-import { DatabaseModule } from "@faker-js/faker/.";
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
   imports: [DatabaseModule],
@@ -13,6 +14,6 @@ import { DatabaseModule } from "@faker-js/faker/.";
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [],
+  providers: [CreateQuestionUseCase],
 })
 export class HttpModule {}
